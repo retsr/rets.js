ec4dde0165ee2f5bd045cc908ef5a1da01883cbb
Danny Graham <good.midget@gmail.com>
2015-04-02 15:40:54 -0700

    implements GetMetadata calls

--- test/index.js
+++ test/index.js
@@ -8,6 +8,7 @@ var RETSError = require('../lib/error');
 var NockURLS = {
     host: 'http://rets.server.com:9160',
     login: '/Login.asmx/Login',
+    getMetadata: '/njs/GetMetadata',
     logout: '/njs/Logout'
 };
 
@@ -41,9 +42,46 @@ var RETSLogoutSuccessResponse = [
     '</RETS>'
 ].join('\n');
 
+var RETSMetadataSuccessResponse = [
+    '<RETS ReplyCode="0" ReplyText="Operation Successful">',
+    '<METADATA>',
+        '<METADATA-RESOURCE Date="2015-03-11T12:37:12" Version="1.00.00010">',
+            '<Resource>',
+                '<ResourceID>Property</ResourceID>',
+                '<StandardName/>',
+                '<VisibleName>Property</VisibleName>',
+                '<Description>Property</Description>',
+                '<KeyField>ListingKey</KeyField>',
+                '<ClassCount>7</ClassCount>',
+                '<ClassVersion>1.00.00006</ClassVersion>',
+                '<ClassDate>2014-12-15T09:29:58</ClassDate>',
+                '<ObjectVersion>1.00.00000</ObjectVersion>',
+                '<ObjectDate>2014-06-20T11:32:55</ObjectDate>',
+                '<SearchHelpVersion>1.00.00000</SearchHelpVersion>',
+                '<SearchHelpDate>2014-06-20T11:32:55</SearchHelpDate>',
+                '<EditMaskVersion>1.00.00000</EditMaskVersion>',
+                '<EditMaskDate>2014-06-20T11:32:55</EditMaskDate>',
+                '<LookupVersion>1.00.00002</LookupVersion>',
+                '<LookupDate>2014-12-15T09:29:58</LookupDate>',
+                '<UpdateHelpVersion>1.00.00000</UpdateHelpVersion>',
+                '<UpdateHelpDate>2014-06-20T11:32:55</UpdateHelpDate>',
+                '<ValidationExpressionVersion>1.00.00000</ValidationExpressionVersion>',
+                '<ValidationExpressionDate>2014-06-20T11:32:55</ValidationExpressionDate>',
+                '<ValidationLookupVersion>1.00.00000</ValidationLookupVersion>',
+                '<ValidationLookupDate>2014-06-20T11:32:55</ValidationLookupDate>',
+                '<ValidationExternalVersion>1.00.00000</ValidationExternalVersion>',
+                '<ValidationExternalDate>2014-06-20T11:32:55</ValidationExternalDate>',
+            '</Resource>',
+        '</METADATA-RESOURCE>',
+    '</METADATA>',
+    '</RETS>'
+].join('\n');
+
 nock(NockURLS.host).persist()
     .get(NockURLS.login)
     .reply(200,RETSLoginSuccessResponse)
+    .get(NockURLS.getMetadata + '?Type=METADATA-RESOURCE&ID=Property&Format=STANDARD-XML')
+    .reply(200,RETSMetadataSuccessResponse)
     .get(NockURLS.logout)
     .reply(200,RETSLogoutSuccessResponse);
 
@@ -191,6 +229,24 @@ describe('RETS Instance Methods',function(){
         assert(rets.session.capabilities.Search && rets.session.capabilities.GetMetadata);
     });
 
+    it('Can get metadata from the server',function(done){
+
+        var timeout = setTimeout(function(){
+            rets.removeAllListeners('metadata');
+            assert(false, 'No event fired');
+            done();
+        },1000);
+
+        rets.addListener('metadata',function(err){
+            rets.removeAllListeners('metadata');
+            clearTimeout(timeout);
+            assert(err === null);
+            done();
+        });
+
+        rets.getMetadata('METADATA-RESOURCE', 'Property');
+    });
+
     it('Can search for property listings: NOT IMPLEMENTED',function(done){
 
         var timeout = setTimeout(function(){
@@ -207,6 +263,24 @@ describe('RETS Instance Methods',function(){
         });
 
         rets.search();
+    });
+
+    it('Can get object from the server: NOT IMPLEMENTED',function(done){
+
+        var timeout = setTimeout(function(){
+            rets.removeAllListeners('object');
+            assert(false, 'No event fired');
+            done();
+        },1000);
+
+        rets.addListener('object',function(err){
+            rets.removeAllListeners('object');
+            clearTimeout(timeout);
+            assert(err.message === 'Not implemented');
+            done();
+        });
+
+        rets.getObject();
     });
 
     it('Can logout of a RETS server',function(done){
@@ -264,6 +338,27 @@ if(fs.existsSync('./test/servers.json')){
 
             it('Can read capabilities from the server', function(){
                 assert(rets.session.capabilities.Search && rets.session.capabilities.GetMetadata);
+            });
+
+            it('Can get metadata from my server',function(done){
+                var metadata = '';
+
+                var timeout = setTimeout(function(){
+                    rets.removeAllListeners('metadata');
+                    assert(false, 'No event fired');
+                    done();
+                },1000);
+
+                rets.addListener('metadata',function(err){
+                    rets.removeAllListeners('metadata');
+                    clearTimeout(timeout);
+                    assert(err === null && metadata !== '');
+                    done();
+                });
+
+                rets.getMetadata('METADATA-RESOURCE', 'Property').on('data',function(line){
+                    metadata += line.toString();
+                });
             });
 
             it('Can logout of my RETS server: ' + rets.session.url.host, function(done){
