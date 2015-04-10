ebc60635c6e0a557a12425f915eaaec12b792681
Danny Graham <good.midget@gmail.com>
2015-04-10 11:47:11 -0700

    extract session from cookies and re-calc RETS-UA-Auth; references #19; git begins moving tests for live servers; _BREAKS_ authentication against some live servers

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
-require('./error.test.js');
-require('./session.test.js');
-require('./rets.test.js');
+// require('./error.test.js');
+// require('./session.test.js');
+// require('./rets.test.js');
+require('./index.old.js');
